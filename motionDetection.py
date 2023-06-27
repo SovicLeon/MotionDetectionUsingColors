c565f4d3c52d800539c387ec2496f93147ab87c6
SovicLeon <sovicleon@gmail.com>
2023-06-27 16:02:45 +0200

    Update motionDetection.py

--- motionDetection.py
+++ motionDetection.py
@@ -1,35 +1,35 @@
 import numpy as np
 import cv2 as cv
 
-# Nastavimo kamero
+# Set up the camera
 cap = cv.VideoCapture(0)
 
-# Pridobimo širino in višino kamere
+# Get the width and height of the camera
 w = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 h = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
-# Naredimo izračun potreben za število korakov pri obedlovanju slike
-h = int(h/2)
-w = int(w/2)
+# Calculate the necessary steps for image processing
+h = int(h / 2)
+w = int(w / 2)
 
-# Koordinate za izbiro barve kože
+# Coordinates for selecting skin color
 xG1 = 0
 yG1 = 0
 xG2 = 0
 yG2 = 0
 
-# Spremenljivke za preverjanje
+# Variables for checking
 flipCoords = False
 setRec = False
 colorSet = False
 
-# Timer za izris pravokotnika za izbiro barve
+# Timer for drawing the color selection rectangle
 timer = 30
 
-# Spremenljivke za meje barve
-spodnjaG = np.ndarray((1,3))
-zgornjaG = np.ndarray((1,3))
+# Variables for color range
+spodnjaG = np.ndarray((1, 3))
+zgornjaG = np.ndarray((1, 3))
 
-# Preverjenje ali smo izračunali število korakov za obdelati sliko in število korakov za obdelati
+# Check if we have calculated the steps for image processing
 stepSet = False
 stepW = 0
 stepH = 0
@@ -37,27 +37,27 @@ steps = 0
 stepW2 = 0
 stepH2 = 0
 
-# Določimo sliko
+# Define the color selection region in the image
 def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
-    # Izberemo sliko
-    (height, width, depth) = slika[levo_zgoraj[0]:desno_spodaj[0],levo_zgoraj[1]:desno_spodaj[1]].shape
-    # Izračunamo povprečje za celo sliko, za vse barve
-    mean = np.mean(slika[levo_zgoraj[0]:desno_spodaj[0],levo_zgoraj[1]:desno_spodaj[1]], axis=(0, 1))
-    # Izračunamo standardni odklon in določimo nižjo in višjo vrednost
+    # Select the image region
+    (height, width, depth) = slika[levo_zgoraj[0]:desno_spodaj[0], levo_zgoraj[1]:desno_spodaj[1]].shape
+    # Calculate the average for the entire region, for all color channels
+    mean = np.mean(slika[levo_zgoraj[0]:desno_spodaj[0], levo_zgoraj[1]:desno_spodaj[1]], axis=(0, 1))
+    # Calculate the standard deviation and determine the lower and upper bounds
     stds = np.std(mean)
-    meanL = mean - 3*stds
-    meanH = mean + 3*stds
-    return (meanL,meanH)
+    meanL = mean - 3 * stds
+    meanH = mean + 3 * stds
+    return (meanL, meanH)
 
-# Zmanjšamo sliko
+# Resize the image
 def zmanjsaj_sliko(slika):
-    obdelanaSlika = slika[h-130:h+130, w-150:w+150]
+    obdelanaSlika = slika[h - 130:h + 130, w - 150:w + 150]
     return obdelanaSlika
 
-# Obdelujemo sliko
-def obdelaj_sliko(slika, okno_sirina, okno_visina,barva_koze_spodaj, barva_koze_zgoraj):
+# Process the image
+def obdelaj_sliko(slika, okno_sirina, okno_visina, barva_koze_spodaj, barva_koze_zgoraj):
     global stepSet, stepW, stepH, steps, stepW2, stepH2
-    # Preverjamo, če smo že delali izračune, da se izvaja hitreje
+    # Check if we have already calculated the steps to speed up execution
     if not stepSet:
         (height, width, depth) = slika.shape
         stepW = width * okno_sirina
@@ -70,34 +70,36 @@ def obdelaj_sliko(slika, okno_sirina, okno_visina,barva_koze_spodaj, barva_koze_
     maxI = 0
     maxJ = 0
     maxIJV = 0
-    # Se premikamo skozi sliko in štejemo piksle s funkcijo
+    # Iterate through the image and count pixels using the given function
     for i in range(steps):
         for j in range(steps):
-            stPiklsov = prestej_piksle_z_barvo_koze(slika[int(stepW2*i):int(stepW2*i+stepW),int(stepH2*j):int(stepH2*j+stepH)],barva_koze_spodaj,barva_koze_zgoraj)
-            # Preverimo, če je največ ujemajočih pikslov na tej podsliki
+            stPiklsov = prestej_piksle_z_barvo_koze(
+                slika[int(stepW2 * i):int(stepW2 * i + stepW), int(stepH2 * j):int(stepH2 * j + stepH)],
+                barva_koze_spodaj, barva_koze_zgoraj)
+            # Check if the current sub-image has the maximum matching pixels
             if stPiklsov > maxIJV:
                 maxI = i
                 maxJ = j
                 maxIJV = stPiklsov
-    # Vrnemo koordinate najbol ujemajoče slike
-    return [int(stepW2*maxJ), int(stepH2*maxI)], [int(stepW2*maxJ+stepW), int(stepH2*maxI+stepH)]
+    # Return the coordinates of the best matching region
+    return [int(stepW2 * maxJ), int(stepH2 * maxI)], [int(stepW2 * maxJ + stepW), int(stepH2 * maxI + stepH)]
 
-# Štejemo piklse v določenih razponih
+# Count pixels within a specified color range
 def prestej_piksle_z_barvo_koze(podslika, barva_koze_spodaj, barva_koze_zgoraj):
-    # Nastavimo masko na vse piksle, ki so v določenih razponih barv
+    # Set the mask to all pixels within the specified color range
     mask = np.all((barva_koze_spodaj <= podslika) & (podslika <= barva_koze_zgoraj), axis=-1)
-    # Preštejemo piksle v maski
+    # Count the pixels in the mask
     numOfPixels = np.sum(mask)
     return numOfPixels
 
-# Nastavimo koordinate za določanje barve, z mouse click
+# Set the coordinates for color selection using mouse click
 def clickDown(event, x, y, flags, param):
     global yG1, xG1, yG2, xG2, flipCoords, setRec
     if event == cv.EVENT_LBUTTONDOWN:
-        print("Click on position ({},{})".format(x,y))
-        # If stavek če izbiramo levo zograj ali desno spodaj
+        print("Click on position ({}, {})".format(x, y))
+        # If statement to select the top left or bottom right coordinate
         if flipCoords:
-            # Če je narobno zaporedje klikov, zamenjamo levo zgoraj in desno spodaj
+            # If the click order is reversed, swap the top left and bottom right coordinates
             if y < yG1:
                 xG2 = xG1
                 xG1 = x
@@ -114,60 +116,53 @@ def clickDown(event, x, y, flags, param):
             flipCoords = True
             setRec = False
 
-# Če se kamera ne odpre
+# If the camera fails to open
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-# Loop za zajemanje slike
+# Image capture loop
 while True:
-    # Beremo sliko
+    # Read the image
     ret, frame = cap.read()
 
-    # Preverimo če smo dobili sliko
+    # Check if we have received the image
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    
-    # Obrnemo sliko
+
+    # Flip the image
     frame = cv.flip(frame, 1)
 
-    # Pomanjšamo sliko
+    # Resize the image
     frame = zmanjsaj_sliko(frame)
 
-    # Preverjamo če rišemo kvadrat za izbiro barve kože
+    # Check if we are drawing a rectangle for skin color selection
     if setRec:
         imgDraw = np.zeros(frame.shape, np.uint8)
-        imgDraw = cv.rectangle(imgDraw, (xG1-1,yG2), (xG2,yG1-1), (0,255,0), 1)
+        imgDraw = cv.rectangle(imgDraw, (xG1 - 1, yG2), (xG2, yG1 - 1), (0, 255, 0), 1)
         frame = frame | imgDraw
         timer -= 1
-        # Ko poteče timer za določanje barve se določi barva in kvadrat se ne riše več
+        # When the timer for color selection expires, determine the color and stop drawing the rectangle
         if timer == 0:
-            spodnjaG, zgornjaG = doloci_barvo_koze(frame,(yG1,xG1),(yG2,xG2))
+            spodnjaG, zgornjaG = doloci_barvo_koze(frame, (yG1, xG1), (yG2, xG2))
             print("Skin color: ({}; {})".format(spodnjaG, zgornjaG))
             setRec = False
             colorSet = True
             timer = 30
 
-    # Če smo določili barvo
+    # If the color is set, process the image
     if colorSet:
-        # Pridobimo dve koordinati najboljšega ujemanja
-        p1, p2 = obdelaj_sliko(frame, 0.3, 0.3, spodnjaG, zgornjaG)
-        # Narišemo kvadrat in nastavimo sliko
-        imgDraw = np.zeros(frame.shape, np.uint8)
-        imgDraw = cv.rectangle(imgDraw, (p1[0], p1[1]), (p2[0], p2[1]), (0, 0, 255), 3)
-        frame = frame | imgDraw
+        koordinatiG1, koordinatiG2 = obdelaj_sliko(frame, 0.4, 0.4, spodnjaG, zgornjaG)
+        frame = cv.rectangle(frame, tuple(koordinatiG1), tuple(koordinatiG2), (0, 0, 255), 2)
 
-    # Prikažemo sliko
+    # Display the resulting frame
     cv.imshow('frame', frame)
 
-    # Callback za mouse click
-    cv.setMouseCallback("frame", clickDown)
-
-    # S tipko q zaustavimo delovanje
-    if cv.waitKey(1) == ord('q'):
+    # Check if the 'q' key is pressed to exit the loop
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Konec delovanja
+# Release the capture and close all windows
 cap.release()
 cv.destroyAllWindows()
